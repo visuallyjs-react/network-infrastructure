88ab6c525feebd7bc78d29a9c8582d0e5fa15d91
Simon Porritt <simon@jsplumbtoolkit.com>
2026-07-09 11:17:26 +1000

    refactored to use the new `useVisuallyJsUpdate` hook to get the value to display

--- src/Total.jsx
+++ src/Total.jsx
@@ -1,25 +1,14 @@
-import { useDiagram } from "@visuallyjs/browser-ui-react"
-import React, {useRef, useState} from "react";
-import { EVENT_DATA_UPDATED, EVENT_GRAPH_CLEARED } from "@visuallyjs/browser-ui"
+import {useVisuallyJsUpdate} from "@visuallyjs/browser-ui-react"
+import React, {useState} from "react";
 
 export default function Total() {
 
     const [total, setTotal] = useState(0)
-    const model = useRef(null)
 
-    function update() {
-        if (model.current) {
-            const nodeTotal = model.current.getNodes().map(n => n.data).reduce((acc, current) => acc + current.monthlyPrice, 0)
-            const groupTotal = model.current.getGroups().map(n => n.data).reduce((acc, current) => acc + current.monthlyPrice, 0)
-            setTotal(nodeTotal + groupTotal)
-        }
-    }
-
-    useDiagram().then(s => {
-        model.current = s.model
-        s.model.bind(EVENT_DATA_UPDATED, update)
-        s.model.bind(EVENT_GRAPH_CLEARED, update)
-        update()
+    useVisuallyJsUpdate((model) => {
+        const nodeTotal = model.getNodes().map(n => n.data).reduce((acc, current) => acc + current.monthlyPrice, 0)
+        const groupTotal = model.getGroups().map(n => n.data).reduce((acc, current) => acc + current.monthlyPrice, 0)
+        setTotal(nodeTotal + groupTotal)
     })
 
     return <div style={{position:"absolute", top:"0.5rem", right:"0.5rem", fontSize:27}}>{'$'}{total.toFixed(2)}</div>
